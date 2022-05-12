4c2f6ebe88ac662396a1316019a045a7a9884b80
cfillies <cfillies@semtalk.com>
2022-05-12 14:00:50 +0200

    s

---
+++ build/images/images.d.ts
@@ -0,0 +1,10 @@
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.gif';
+declare module '*.xml';
+declare module '*.svg';
+declare module "*.json" {
+    const value: any;
+    export default value;
+}
